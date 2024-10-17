14985197b316b7f33a65752f492aa7b059a4c247
eisax <eisaxoffice@gmail.com>
2024-10-18 01:49:32 +0200

    feat:added edit salon screen

---
+++ lib/view/screens/settings/widgets/edit_salon_details/edit_salon_details_screen.dart
@@ -0,0 +1,349 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:mbs/util/dimensiona.dart';
+import 'package:mbs/util/images.dart';
+import 'package:mbs/view/widgets/custom_textinput_widget.dart';
+
+class EditSalonDetailsScreen extends StatefulWidget {
+  const EditSalonDetailsScreen({super.key});
+
+  @override
+  State<EditSalonDetailsScreen> createState() => _EditSalonDetailsScreenState();
+}
+
+class _EditSalonDetailsScreenState extends State<EditSalonDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFf4f4f4),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 18,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: Dimensions.paddingSizeExtraSmall,
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  Get.back();
+                },
+                child: SvgPicture.asset(
+                  Images.arrowback,
+                  height: 30,
+                  width: 30,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: Dimensions.paddingSizeSmall,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: Dimensions.paddingSizeDefault,
+              ),
+              child: Text(
+                "Edit Salon Details",
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    fontSize: Dimensions.fontSizeExtraLarge,
+                    fontWeight: FontWeight.w700,
+                    color: Theme.of(context).primaryColor,
+                    letterSpacing: 1),
+              ),
+            ),
+            SizedBox(
+                            height: Dimensions.paddingSizeDefault,
+                          ),
+            Expanded(
+              child: Container(
+                color: Colors.white,
+                child: Expanded(
+                  child: SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Container(
+                      width: Get.width,
+                      padding: EdgeInsets.all(
+                        Dimensions.paddingSizeDefault,
+                      ),
+                      
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              
+                              Text(
+                                "Salon Name",
+                                style:
+                                    Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                          fontSize: Dimensions.fontSizeLarge,
+                                          fontWeight: FontWeight.w400,
+                                          letterSpacing: 1,
+                                        ),
+                              ),
+                              SizedBox(
+                                height: Dimensions.paddingSizeSmall,
+                              ),
+                              CustomTextField(
+                                borderRadius: Dimensions.radiusSmall,
+                                hintText: 'Rocky : ',
+                                controller: TextEditingController(),
+                                fillColor:
+                                    Theme.of(context).hintColor.withOpacity(0.25),
+                                inputType: TextInputType.emailAddress,
+                                // errorText: "Please enter your email",
+                                borderColor: null,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: Dimensions.paddingSizeLarge,
+                              ),
+                              Text(
+                                "About Salon",
+                                style:
+                                    Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                          fontSize: Dimensions.fontSizeLarge,
+                                          fontWeight: FontWeight.w400,
+                                          letterSpacing: 1,
+                                        ),
+                              ),
+                              SizedBox(
+                                height: Dimensions.paddingSizeSmall,
+                              ),
+                              CustomTextField(
+                                borderRadius: Dimensions.radiusSmall,
+                                hintText: 'About Salon',
+                                controller: TextEditingController(),
+                                fillColor:
+                                    Theme.of(context).hintColor.withOpacity(0.25),
+                                inputType: TextInputType.emailAddress,
+                                // errorText: "Please enter your email",
+                                borderColor: null,
+                                maxLines: 5,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: Dimensions.paddingSizeLarge,
+                              ),
+                              Text(
+                                "Salon Address",
+                                style:
+                                    Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                          fontSize: Dimensions.fontSizeLarge,
+                                          fontWeight: FontWeight.w400,
+                                          letterSpacing: 1,
+                                        ),
+                              ),
+                              SizedBox(
+                                height: Dimensions.paddingSizeSmall,
+                              ),
+                              CustomTextField(
+                                borderRadius: Dimensions.radiusSmall,
+                                hintText: 'Salon Address',
+                                controller: TextEditingController(),
+                                fillColor:
+                                    Theme.of(context).hintColor.withOpacity(0.25),
+                                inputType: TextInputType.emailAddress,
+                                // errorText: "Please enter your email",
+                                borderColor: null,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: Dimensions.paddingSizeLarge,
+                              ),
+                              Text(
+                                "Phone Number",
+                                style:
+                                    Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                          fontSize: Dimensions.fontSizeLarge,
+                                          fontWeight: FontWeight.w400,
+                                          letterSpacing: 1,
+                                        ),
+                              ),
+                              SizedBox(
+                                height: Dimensions.paddingSizeSmall,
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(
+                                    Dimensions.radiusSmall,
+                                  ),
+                                  color:
+                                      Theme.of(context).hintColor.withOpacity(0.25),
+                                ),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      decoration: const BoxDecoration(
+                                        borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(
+                                            Dimensions.radiusSmall,
+                                          ),
+                                          topLeft: Radius.circular(
+                                            Dimensions.radiusSmall,
+                                          ),
+                                        ),
+                                        color: Color(0xFF121212),
+                                      ),
+                                      child: CountryCodePicker(
+                                        onChanged: print,
+                                        initialSelection: 'IT',
+                                        favorite: const ['+263', 'ZW'],
+                                        showCountryOnly: false,
+                                        showOnlyCountryWhenClosed: false,
+                                        alignLeft: false,
+                                        textStyle: Theme.of(context)
+                                            .textTheme
+                                            .bodyMedium
+                                            ?.copyWith(
+                                              fontSize: Dimensions.fontSizeLarge,
+                                              fontWeight: FontWeight.w400,
+                                              letterSpacing: 1,
+                                              color: Colors.white,
+                                            ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: CustomTextField(
+                                        borderRadius: Dimensions.radiusSmall,
+                                        hintText: '774 123 456',
+                                        controller: TextEditingController(),
+                                        fillColor: Colors.transparent,
+                                        inputType: TextInputType.emailAddress,
+                                        // errorText: "Please enter your email",
+                                        borderColor: null,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: Dimensions.paddingSizeLarge,
+                              ),
+                              Text(
+                                "Salon Location",
+                                style:
+                                    Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                          fontSize: Dimensions.fontSizeLarge,
+                                          fontWeight: FontWeight.w400,
+                                          letterSpacing: 1,
+                                        ),
+                              ),
+                              SizedBox(
+                                height: Dimensions.paddingSizeSmall,
+                              ),
+                              Center(
+                                child: SizedBox(
+                                  height: 50,
+                                  width: Get.width,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      elevation: 0,
+                                      backgroundColor: const Color(0xFFC8E6C9),
+                                      textStyle: const TextStyle(
+                                          color: Colors.white,
+                                          fontStyle: FontStyle.normal),
+                                      shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(
+                                            Dimensions.radiusSmall,
+                                          ),
+                                        ),
+                                      ),
+                                      shadowColor: Theme.of(context).primaryColor,
+                                    ),
+                                    onPressed: () async {},
+                                    child: Text(
+                                      'Submit',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall
+                                          ?.copyWith(
+                                            color: Colors.green,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: Dimensions.paddingSizeDefault,
+                              ),
+                              Center(
+                                child: SizedBox(
+                                  height: 50,
+                                  width: Get.width,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      elevation: 0,
+                                      backgroundColor: Theme.of(context).primaryColor,
+                                      textStyle: const TextStyle(
+                                          color: Colors.white,
+                                          fontStyle: FontStyle.normal),
+                                      shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(
+                                            Dimensions.radiusSmall,
+                                          ),
+                                        ),
+                                      ),
+                                      shadowColor: Theme.of(context).primaryColor,
+                                    ),
+                                    onPressed: () async {},
+                                    child: Text(
+                                      'Submit',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall
+                                          ?.copyWith(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                  
+                    ),
+                  ),
+                ),
+              ),
+            )
+          
+          
+          ],
+        ),
+      ),
+    );
+  }
+}
